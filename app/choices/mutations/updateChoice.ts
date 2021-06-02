863edbe32898edf2873d23475c010bd8f89e565d
Michele Memoli <michele@100shapes.com>
2021-06-02 13:27:07 +0100

    Added ability to vote on choices

--- app/choices/mutations/updateChoice.ts
+++ app/choices/mutations/updateChoice.ts
@@ -4,16 +4,11 @@ import { z } from "zod"
 
 const UpdateChoice = z.object({
   id: z.number(),
-  name: z.string(),
 })
 
-export default resolver.pipe(
-  resolver.zod(UpdateChoice),
-  resolver.authorize(),
-  async ({ id, ...data }) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const choice = await db.choice.update({ where: { id }, data })
+export default resolver.pipe(resolver.zod(UpdateChoice), resolver.authorize(), async ({ id }) => {
+  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+  const choice = await db.choice.update({ where: { id }, data: { votes: { increment: 1 } } })
 
-    return choice
-  }
-)
+  return choice
+})
